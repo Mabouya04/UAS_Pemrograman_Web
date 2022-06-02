4da96bb09c1d08b6556e092dae122d21564ffe91
muhammadhidayatullah17 <2010817110012@mhs.ulm.ac.id>
2022-06-02 16:13:54 +0800

    update nim & email in about

--- app/Models/About.php
+++ app/Models/About.php
@@ -7,7 +7,7 @@ class About{
     private static 
     $data = [
         ['nama'=>'Muhammad Ilham Nor Ramadhani', 'nim'=> '2010817110008', 'email'=> '2010817110008@mhs.ulm.ac.id'],
-        ['nama'=>'Muhammad Hidayatullah', 'nim'=> '2010817110008', 'email'=> '2010817110008@mhs.ulm.ac.id'],
+        ['nama'=>'Muhammad Hidayatullah', 'nim'=> '2010817110012', 'email'=> '2010817110012@mhs.ulm.ac.id'],
         ['nama'=>'Muhammad Hasan Adli', 'nim'=> '2010817110008', 'email'=> '2010817110008@mhs.ulm.ac.id']];
 
     public static function getAll(){
