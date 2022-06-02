6d0589e685d21203e05e72e7f60906bd3ca96150
muhammadhidayatullah17 <2010817110012@mhs.ulm.ac.id>
2022-06-02 16:05:29 +0800

    Update About

--- app/Models/About.php
+++ app/Models/About.php
@@ -8,7 +8,7 @@ class About{
     $data = [
         ['nama'=>'Muhammad Ilham Nor Ramadhani', 'nim'=> '2010817110008', 'email'=> '2010817110008@mhs.ulm.ac.id'],
         ['nama'=>'Muhammad Hidayatullah', 'nim'=> '2010817110008', 'email'=> '2010817110008@mhs.ulm.ac.id'],
-        ['nama'=>'Hasan Adli', 'nim'=> '2010817110008', 'email'=> '2010817110008@mhs.ulm.ac.id']];
+        ['nama'=>'Muhammad Hasan Adli', 'nim'=> '2010817110008', 'email'=> '2010817110008@mhs.ulm.ac.id']];
 
     public static function getAll(){
         return collect(self::$data);
