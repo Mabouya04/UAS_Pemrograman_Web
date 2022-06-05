bd1052a5cdfbe9e8f3bdad69284e572a8723230e
mabouya04 <iilbarcelona9@gmail.com>
2022-06-05 13:21:50 +0800

    Repairing Missing file

--- resources/views/tipes.blade.php
+++ resources/views/tipes.blade.php
@@ -5,7 +5,7 @@
     <ul class="list-group">
     @foreach ($tipeSenjatas as $tipe)
                 
-        <li class="list-group-item"><h1><a href="/tipes/{{ $tipe->slug }}" class="link-primary text-decoration-none">{{ $tipe->name }}</a></h1></li>
+        <li class="list-group-item"><h2><a href="/tipes/{{ $tipe->slug }}" class="link-primary text-decoration-none">{{ $tipe->name }}</a></h2></li>
                 
     @endforeach
     </ul>
